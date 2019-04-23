d210c0e0b3c1360927ef5f4c232f4a40661de045
rbb091020 <41700255+rbb091020@users.noreply.github.com>
2019-04-22 20:54:54 -0500

    implementation with sedick's menu

--- window.cpp
+++ window.cpp
@@ -10,6 +10,13 @@ void Window::start(){
     //cbreak();
     initscr();
     raw();
+    //noecho();
+
+    /*int main_height= 100;
+    int main_width = 300;
+    int start_y = 0, start_x = 0;
+    
+    */
     getmaxyx(stdscr,main_height,main_width);
     int start_y = 0, start_x = 0;
 
@@ -81,8 +88,9 @@ void Window::start(){
     box(main_win, 0, 0);
     wrefresh(main_win);
 
+
     controller.setup(user);
-    main_page(user,main_win);
+    main_page(user, main_win);
     endwin();
 }
 
@@ -97,49 +105,46 @@ void Window::main_page(User* user, WINDOW* main){
     int grid_height = main_height/2;
     int grid_width = main_width/2 - 4;
     string user_colon = user->getUsername() + ": ";
-    string choices[4] = {"Join Chat", "Create Chat", "Delete Chat", "Change Username"};
-
+    string choices[4] = {"1 - Join Chat", "2 - Create Chat", "3 - Delete Chat", "4 - Change Username"};
 
     WINDOW* chatroom_grid = newwin(grid_height, grid_width, 1, 1);
     WINDOW* lobby_grid = newwin(grid_height, grid_width, 1, main_width/2 + 2);
-    WINDOW* menu_grid = newwin(grid_height/2, grid_width, grid_height + 1, 1);
-    WINDOW* message_box = newwin(grid_height * .2, grid_width - 4, grid_height - grid_height* .2, grid_width + 8);
-    WINDOW* output_box = newwin(grid_height/2, grid_width, grid_height + 1, main_width/2 + 2);
-    WINDOW* input_box = newwin(grid_height * .2, grid_width - 4, grid_height + grid_height/2 + 1, grid_width + 8);
+    WINDOW* menu_grid = newwin(grid_height/2+1, grid_width, grid_height + 1, 1);
+    WINDOW* input_box = newwin(grid_height * .2, grid_width - 4, grid_height - grid_height* .2, grid_width + 8);
     refresh();
+    //wrefresh(main);
     box(chatroom_grid, 0, 0);
     box(lobby_grid, 0, 0);
-    box(menu_grid, 0, 0);
-    box(message_box, 0, 0);
-    box(output_box, 0, 0);
+    //box(menu_grid, 0, 0);
     box(input_box, 0, 0);
     wattron(chatroom_grid, A_BLINK); 
     mvwprintw(chatroom_grid, 1, grid_width / 2, "CHATROOMS"); 
     wattroff(chatroom_grid,A_BLINK); 
     mvwprintw(lobby_grid, 1, grid_width/2, "LOBBY");
-    mvwprintw(menu_grid, 1, grid_width/2, "MENU");
-    mvwprintw(message_box, (grid_height* .2)/2, 1, user_colon.c_str());
+    //mvwprintw(menu_grid, 1, grid_width/2, "MENU");
+    mvwprintw(input_box, (grid_height* .2)/2, 1, user_colon.c_str());
 
-    //wmove(menu_grid,1,1);
-    //wmove(output_box, 1, 1);
+    wmove(menu_grid,1,1);
     refresh();
+    //wrefresh(main);    
 
-    wrefresh(input_box);
+    
     wrefresh(chatroom_grid);
     wrefresh(lobby_grid);
     wrefresh(menu_grid);
-    wrefresh(message_box);
-    wrefresh(output_box);
+    wrefresh(input_box);
     refresh();
-    /*wmove(menu_grid,2, 1);
+    wmove(menu_grid,2, 1);
     refresh();
     wrefresh(menu_grid);
-    keypad(menu_grid, true);
-    */
+    //keypad(menu_grid, true);
     int c;
-    int i = 0;
+    int highlight = 0;
+    int selection;
+    int menu_mode = 1, chatroom_mode = 0, temp, esc_pressed = 0;
+
     //prints list the first time
-    for(i = 0; i < 4; i++){
+/*    for(int i = 0; i < 4; i++){
         
         if(i == 0){
             wattron(menu_grid,A_REVERSE);
@@ -148,57 +153,108 @@ void Window::main_page(User* user, WINDOW* main){
             wattroff(menu_grid,A_REVERSE);
         mvwprintw(menu_grid, i + 2, 1, choices[i].c_str());
         
-    }
-    wrefresh(output_box);
-    mvwprintw(input_box, 1, (grid_width - 4)/ 2 , "in the inputbox");
-    wrefresh(input_box);
-    int a = getch();
-
-    if(a == 'a'){
-        mvwprintw(output_box, 2, 1, "Enter chatroom name");
-        wrefresh(output_box);
-
-        wmove(input_box, 2, 1);
-        wrefresh(input_box);
-        getch();
-    }
-    //wgetch(output_box);
-    i = 0;
-    wrefresh(output_box);
-    wrefresh(menu_grid);
-    //getch();
-    //27 is the escape key
-    /*while (c = wgetch(menu_grid) != 27){
-        mvwprintw(menu_grid, i + 2, 1, choices[i].c_str());
-        
-            switch( c ) {
-                case KEY_UP:
-                    i--;
-                    if(i == -1){
-                        i = 4;
-                    }
-                    break;
-                case KEY_DOWN:
-                    i++;
-                    if(i == 5){
-                        i = 0;
-                    }
-                    break;
-                case KEY_RIGHT: 
-                    lobby_chat(input_box);
-                    break;
-            }
-            // now highlight the next item in the list.
-            wattron( menu_grid, A_REVERSE );
-            mvwprintw(menu_grid, i+2, 1, choices[i].c_str());
-            wrefresh(menu_grid);
-            wattroff( menu_grid, A_REVERSE );
     }*/
-}
+    int i = 0;
+    wrefresh(menu_grid);
+    noecho();
+    curs_set(false);
+    //27 is the escape key 
+    //While either chatroom mode or menu mode && esc key is not pressed
+    while (chatroom_mode || menu_mode)
+    {
+        int menu_printed = 0;
 
-void Window::lobby_chat(WINDOW* input_box){
-    wmove(input_box, 1, 1);
-    refresh();
-    wrefresh(input_box);
-    getch(); 
+        while (chatroom_mode){
+            c = wgetch(menu_grid);
+            //if tab is pressed
+            if(c == 9 ){
+                //temp = menu_mode; 
+                menu_mode = 1;
+                chatroom_mode = 0;
+                
+            }
+            //if escape is pressed             
+            if(c == 27 ){
+                //turns off menu and chatroom mode, exiting the window
+                chatroom_mode = 0;
+                menu_mode = 0;
+            }
+        }
+        while (menu_mode){
+                if(!menu_printed)
+                {
+                    mvwprintw(menu_grid, 1, grid_width/2, "MENU");
+                    box(menu_grid, 0, 0);
+                    for(int i = 0; i < 4; i++){
+                            wattroff(menu_grid,A_REVERSE);
+                        mvwprintw(menu_grid, i + 2, 1, choices[i].c_str());
+                   
+                    }
+                    int i = 0;
+                    wrefresh(menu_grid);  
+                     menu_printed = 1;   
+
+                    
+                }
+                c = wgetch(menu_grid);
+                //if tab is pressed
+                if(c == 9){
+                    //Turn off menu_mode and cha
+                    //temp = menu_mode;
+                    menu_mode = 0;
+                    chatroom_mode = 1;
+                    werase(menu_grid);
+                    wrefresh(menu_grid);
+                } 
+                //if escape is pressed
+                if(c == 27 ){
+                    //turns off menu and chatroom mode, exiting the window
+                    chatroom_mode = 0;
+                    menu_mode = 0;
+                }
+                //1 - Join Chatroom
+                //2 - Create Chatroom
+                //3 - Delete Chatroom
+                //4 - Change Username  
+                switch( c ) {
+                    //Join Chatroom
+                    case 1:
+                     //           chatroom_mode = 0;
+                    //menu_mode = 0;
+                                //Controller.add_user_to_chatroom()
+                                break;
+                    //Create Chatroom
+                    case 2:
+                    //chatroom_mode = 0;
+                    //menu_mode = 0;     
+                                //Controller.create_chatroom()
+
+                                break;
+                    //Delete Chatroom
+                    case 3:
+                    //chatroom_mode = 0;
+                    //menu_mode = 0;
+                                break;
+                    //Change Username
+                    case 4:
+                    //chatroom_mode = 0;
+                   // menu_mode = 0;
+                                //string new_Username;
+                                //Controller.edit_username(username,)
+                                break;                                        
+                    default:
+                                break;
+                                }
+
+                // now highlight the next item in the list.
+    /*            wattron( menu_grid, A_REVERSE );
+                wrefresh(menu_grid);
+                mvwprintw(menu_grid, i+2, 1, choices[i].c_str());
+                wattroff( menu_grid, A_REVERSE );
+                wrefresh(menu_grid);*/
+        }
+
+
+    //getch();
+}
 }
